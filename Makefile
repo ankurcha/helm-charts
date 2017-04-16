37a79c54a62352c63bf5c4d590f950453d8fc058
Shane Ramey <shane.ramey@gmail.com>
2017-04-15 21:32:43 -0700

    update helm charts + Makefile

--- Makefile
+++ Makefile
@@ -1,3 +1,22 @@
+HELM_CHART_NAME := please_pass_HELM_CHART_NAME
+DOCKER_REPO := us.gcr.io/downup-164000
+HELM_CHART_REPO := http://charts.downup.us
+HELM_REPO_NAME := charts.downup.us
+HELM_REPO_UPLOADER_ENDPOINT := http://localhost:8080/charts
+
+.PHONY: lint_helm bump_version_and_publish_helm_chart_to_repo publish_helm
+
 installstarters:
 	cp -ir helm-starter-charts/* ~/.helm/starters
+
+lint_helm:
+	helm lint $(HELM_CHART_NAME)/
+
+bump_version_and_publish_helm_chart_to_repo:
+	helm repo add $(HELM_REPO_NAME) $(HELM_CHART_REPO)
+
+	helm local_bump -f ${CURDIR}/$(HELM_CHART_NAME)/Chart.yaml --bump-level=patch
+	./publish_helm.sh $(HELM_CHART_NAME)
+
+publish_helm: bump_version_and_publish_helm_chart_to_repo
 
---
+++ publish_helm.sh
@@ -0,0 +1,29 @@
+#! /usr/bin/env bash
+
+if [ -z ${1+x} ]; then
+  echo "Usage: publish_helm.sh chart_name";
+  exit 1
+fi
+
+HELM_CHART_NAME=$1
+
+DOCKER_REPO=us.gcr.io/downup-164000
+HELM_CHART_REPO=http://charts.downup.us
+HELM_REPO_NAME=charts.downup.us
+HELM_REPO_UPLOADER_ENDPOINT=http://localhost:8080/charts
+
+helm repo add $HELM_REPO_NAME $HELM_CHART_REPO
+
+mkdir -p $PWD/$HELM_CHART_NAME/build
+rm -f $PWD/$HELM_CHART_NAME/build/${HELM_CHART_NAME}-*.tgz
+
+# TODO: signing of charts
+# helm package --sign --key 'D84669B161B026671A86575290FBE1F81D70695D' \
+#       --keyring ~/.gnupg/secring.gpg -d build/ $(HELM_CHART_NAME)/
+
+export HELM_PACKAGE_FILE=`helm package $PWD/$HELM_CHART_NAME -d $PWD/$HELM_CHART_NAME/build --debug --save=false | awk '{ print $2 }'`
+echo HELM_PACKAGE_FILE=$HELM_PACKAGE_FILE
+curl -i -X PUT -F repo=default -F chart=@$HELM_PACKAGE_FILE \
+	$HELM_REPO_UPLOADER_ENDPOINT # preferably inside the cluster, but doesn't have to be
+rm $HELM_PACKAGE_FILE
+
